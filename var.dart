01782861ce8beeaf8b8bea8f5314339b73f6ab40
Adi Prasetyo <68819530+adeeep14@users.noreply.github.com>
2022-02-04 13:41:07 +0700

    Create var.dart

---
+++ var.dart
@@ -0,0 +1,15 @@
+void main(){
+  // TipeData Variabel;
+  int angka = 10;
+  double ipk = 3.9;
+  String namaPanjang = 'Adi Prasetyo';
+  bool lulus = true;
+  var alamat = 'Jalan Mawar No.1';
+  
+  print(angka);
+  print(ipk);
+  print(namaPanjang);
+  print(lulus);
+  print(alamat);
+  print('Nama Saya: $namaPanjang \nUmur : $angka');
+}
